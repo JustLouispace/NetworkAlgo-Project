67d17bb318bb667e0fa4c7901b304feef4abdaf2
Louisrelax <112146664+Louisrelax@users.noreply.github.com>
2023-05-31 22:15:32 +0700

    master

--- Main.py
+++ Main.py
@@ -1,81 +1,111 @@
-import threading
 import socket
+import threading
 import time
-import os
+from typing import List
 
-CITY_ID = int(os.environ["CITY_ID"])
-PORT = int(os.environ["PORT"])
 NUM_CITIES = 4
 PORTS = [5001, 5002, 5003, 5004]
 
 class Node(threading.Thread):
-    def __init__(self, city, port, distances):
+    def __init__(self, city, port, lock, distances):
         threading.Thread.__init__(self)
         self.city = city
         self.port = port
-        self.distances = distances
-        self.neighbors = ["node " + str(i+1) for i in range(NUM_CITIES) if i+1 != city]
-        self.isSent = False
+        self.lock = lock
+        self.neighbors = []
         self.receivedMessages = []
+        self.isSent = False
+        self.distances = distances
         self.path = []
 
+        for i in range(NUM_CITIES):
+            if i + 1 != city:
+                self.neighbors.append("node " + str(i + 1))
+
     def run(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('0.0.0.0', self.port))
-            if not self.isSent:
-                for neighbor in self.neighbors:
-                    self.sendMessage(neighbor, str(self.city))
-                self.isSent = True
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+            serverSocket.bind(('localhost', self.port))
+            serverSocket.listen()
+            print(f"\n[Info] Node {self.city} is running on port {self.port}\n")
+
+            with self.lock:
+                if not self.isSent:
+                    for i in range(1, NUM_CITIES + 1):
+                        if i != self.city:
+                            self.sendMessage("node " + str(i), str(self.city))
+                    self.isSent = True
 
             while True:
-                conn, addr = s.accept()
-                data = conn.recv(1024)
-                if data:
-                    self.receivedMessages.append(data.decode())
-                    if len(self.receivedMessages) == NUM_CITIES - 1:
-                        print(f"[Received] Node {self.city} received: {self.receivedMessages}")
-                        break
+                clientSocket, addr = serverSocket.accept()
+                message = clientSocket.recv(1024).decode('utf-8')
+                self.receivedMessages.append(message)
+
+                if len(self.receivedMessages) == NUM_CITIES - 1:
+                    print(f"[Received] Node {self.city} received: {self.receivedMessages}")
+                    break
 
     def sendMessage(self, neighbor, message):
         neighborInfo = neighbor.split(" ")
+        neighborHost = "localhost"
         neighborPort = PORTS[int(neighborInfo[1]) - 1]
-        for i in range(5):  # Retry 5 times
-            try:
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.connect((neighborInfo[0] + neighborInfo[1], neighborPort))
-                    s.sendall(message.encode())
-                    time.sleep(2)
-                break
-            except ConnectionRefusedError:
-                print(f"Connection failed. Retrying in {2 ** i} seconds.")
-                time.sleep(2 ** i)  # Exponential backoff
 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketClient:
+            socketClient.connect((neighborHost, neighborPort))
+            socketClient.sendall(message.encode('utf-8'))
 
-    def calculateDistances(self):
+        time.sleep(2)
+
+    def calculateDistances(self, distances):
         totalDistance = 0
         self.path.append(self.city)
         currentCity = self.city
-        unvisitedCities = [i+1 for i in range(NUM_CITIES) if i+1 != self.city]
+        unvisitedCities = []
+
+        for i in range(NUM_CITIES):
+            if i + 1 != self.city:
+                unvisitedCities.append(i + 1)
 
         while unvisitedCities:
-            nextCity = unvisitedCities.pop(0)
-            distanceToNextCity = self.distances[nextCity-1]
+            nextCity = unvisitedCities.pop(0) # Take the first city from the list of unvisited cities
+            distanceToNextCity = distances[currentCity-1][nextCity-1]
             totalDistance += distanceToNextCity
-            self.path.append(nextCity)
             print(f"[Step] Distance from city {currentCity} to city {nextCity}: {distanceToNextCity}")
             print(f"[Step] Cumulative total distance for node {self.city}: {totalDistance}")
             currentCity = nextCity
+            self.path.append(currentCity)
 
-        # Add the distance to return to the original city
-        distanceToOrigin = self.distances[self.city-1]
-        totalDistance += distanceToOrigin
-        self.path.append(self.city)
-        print(f"[Step] Distance from city {currentCity} to city {self.city}: {distanceToOrigin}")
+        distanceToStartCity = distances[currentCity-1][self.city-1] # Distance back to the starting city
+        totalDistance += distanceToStartCity
+        print(f"[Step] Distance from city {currentCity} back to city {self.city}: {distanceToStartCity}")
         print(f"[Step] Cumulative total distance for node {self.city}: {totalDistance}")
+        self.path.append(self.city) # Add the starting city back to the path
 
-distances = [[0, 10, 15, 20], [10, 0, 35, 25], [15, 35, 0, 30], [20, 25, 30, 0]]
-node = Node(CITY_ID, PORT, distances[CITY_ID-1])
-node.start()
-node.join()
-node.calculateDistances()
-print("Path:", node.path)
+        print(f"[Neighbors] Node {self.city} has neighbors: {self.neighbors}")
+        print(f"[Path] Path for node {self.city}: {self.path}")
+        print(f"[Total Distance] Total distance for node {self.city}: {totalDistance}\n")
+
+def main():
+    nodes = []
+    lock = threading.Lock()
+
+    distances = [
+        [0, 10, 15, 20],
+        [10, 0, 35, 25],
+        [15, 35, 0, 30],
+        [20, 25, 30, 0]
+    ]
+
+    for i in range(NUM_CITIES):
+        port = PORTS[i]
+        node = Node(i + 1, port, lock, distances[i])
+        nodes.append(node)
+        node.start()
+
+    for node in nodes:
+        node.join()
+
+    for node in nodes:
+        node.calculateDistances(distances)
+
+if __name__ == "__main__":
+    main()
