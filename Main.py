ac653ea792e5298a6024b44b07153c08b96704c8
Louisrelax <112146664+Louisrelax@users.noreply.github.com>
2023-05-31 14:19:18 +0700

    ีunFininsh

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
 import threading
 import socket
 import time
+import os
 
+CITY_ID = int(os.environ["CITY_ID"])
+PORT = int(os.environ["PORT"])
 NUM_CITIES = 4
 PORTS = [5001, 5002, 5003, 5004]
 
 class Node(threading.Thread):
-    def __init__(self, city, port, lock, distances):
+    def __init__(self, city, port, distances):
         threading.Thread.__init__(self)
         self.city = city
         self.port = port
-        self.lock = lock
         self.distances = distances
         self.neighbors = ["node " + str(i+1) for i in range(NUM_CITIES) if i+1 != city]
         self.isSent = False
@@ -19,16 +21,11 @@ class Node(threading.Thread):
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('localhost', self.port))
-            self.lock.acquire()
-            try:
-                if not self.isSent:
-                    for i in range(1, NUM_CITIES+1):
-                        if i != self.city:
-                            self.sendMessage("node " + str(i), str(self.city))
-                    self.isSent = True
-            finally:
-                self.lock.release()
+            s.bind(('0.0.0.0', self.port))
+            if not self.isSent:
+                for neighbor in self.neighbors:
+                    self.sendMessage(neighbor, str(self.city))
+                self.isSent = True
 
             while True:
                 conn, addr = s.accept()
@@ -42,12 +39,19 @@ class Node(threading.Thread):
     def sendMessage(self, neighbor, message):
         neighborInfo = neighbor.split(" ")
         neighborPort = PORTS[int(neighborInfo[1]) - 1]
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('localhost', neighborPort))
-            s.sendall(message.encode())
-            time.sleep(2)
+        for i in range(5):  # Retry 5 times
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((neighborInfo[0] + neighborInfo[1], neighborPort))
+                    s.sendall(message.encode())
+                    time.sleep(2)
+                break
+            except ConnectionRefusedError:
+                print(f"Connection failed. Retrying in {2 ** i} seconds.")
+                time.sleep(2 ** i)  # Exponential backoff
 
-    def calculateDistances(self, distances):
+
+    def calculateDistances(self):
         totalDistance = 0
         self.path.append(self.city)
         currentCity = self.city
@@ -55,22 +59,23 @@ class Node(threading.Thread):
 
         while unvisitedCities:
             nextCity = unvisitedCities.pop(0)
-            distanceToNextCity = distances[currentCity-1][nextCity-1]
+            distanceToNextCity = self.distances[nextCity-1]
             totalDistance += distanceToNextCity
+            self.path.append(nextCity)
             print(f"[Step] Distance from city {currentCity} to city {nextCity}: {distanceToNextCity}")
             print(f"[Step] Cumulative total distance for node {self.city}: {totalDistance}")
             currentCity = nextCity
 
-def main():
-    distances = [[0, 10, 15, 20], [10, 0, 35, 25], [15, 35, 0, 30], [20, 25, 30, 0]]
-    lock = threading.Lock()
-    nodes = [Node(i+1, PORTS[i], lock, distances[i]) for i in range(NUM_CITIES)]
-    for node in nodes:
-        node.start()
-    for node in nodes:
-        node.join()
-    for node in nodes:
-        node.calculateDistances(distances)
+        # Add the distance to return to the original city
+        distanceToOrigin = self.distances[self.city-1]
+        totalDistance += distanceToOrigin
+        self.path.append(self.city)
+        print(f"[Step] Distance from city {currentCity} to city {self.city}: {distanceToOrigin}")
+        print(f"[Step] Cumulative total distance for node {self.city}: {totalDistance}")
 
-if __name__ == "__main__":
-    main()
+distances = [[0, 10, 15, 20], [10, 0, 35, 25], [15, 35, 0, 30], [20, 25, 30, 0]]
+node = Node(CITY_ID, PORT, distances[CITY_ID-1])
+node.start()
+node.join()
+node.calculateDistances()
+print("Path:", node.path)
--- dockerfile
+++ dockerfile
@@ -1,5 +1,14 @@
-FROM openjdk:11
-COPY . /app
+# Use an official Python runtime as a parent image
+FROM python:3.9
+
+# Set the working directory in the container to /app
 WORKDIR /app
-RUN javac Main.java
-CMD ["java", "Main"]
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
+
+# Run main.py when the container launches
+CMD ["python", "main.py"]
